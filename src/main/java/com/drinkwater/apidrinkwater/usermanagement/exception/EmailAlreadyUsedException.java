53a0e43b04a6921be464b19d3d79fdacc6808111
eagle-head <edy.kohn@gmail.com>
2024-04-07 12:24:26 -0300

    Add serialVersionUID in EmailAlreadyUsedException.

--- src/main/java/com/drinkwater/apidrinkwater/usermanagement/exception/EmailAlreadyUsedException.java
+++ src/main/java/com/drinkwater/apidrinkwater/usermanagement/exception/EmailAlreadyUsedException.java
@@ -1,6 +1,11 @@
 package com.drinkwater.apidrinkwater.usermanagement.exception;
 
+import java.io.Serial;
+
 public class EmailAlreadyUsedException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public EmailAlreadyUsedException(String message) {
         super(message);
